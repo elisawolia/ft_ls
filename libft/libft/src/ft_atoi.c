2d300c79f3d764338a14f4dbe3b32e043e7fcc65
Aleksandr <a-gerasimov85@mail.ru>
2020-07-14 18:22:09 +0300

    leakfix in atoi

--- libft/libft/src/ft_atoi.c
+++ libft/libft/src/ft_atoi.c
@@ -12,38 +12,43 @@
 
 #include "libft.h"
 
-static	void	sign(char **s, int *z)
+static int	check_symbols(const char *str)
 {
-	if (*(*s) == '-' || *(*s) == '+')
+	size_t i;
+
+	i = 0;
+	while (str[i] == '\n' || str[i] == ' ' || str[i] == '\t' ||
+			(str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
 	{
-		if (*(*s) == '-')
-			*z = -1;
-		(*s)++;
+		i++;
 	}
+	return (i);
 }
 
-int				ft_atoi(const char *str)
+int			ft_atoi(const char *str)
 {
-	unsigned long long	nb;
-	int					z;
-	char				*s;
+	unsigned long long	num;
+	size_t				i;
+	int					minus;
 
-	s = (char *)malloc(sizeof(char *) * ft_strlen(str) + 1);
-	if (s == NULL)
-		return (0);
-	nb = 0;
-	s = (char *)str;
-	z = 1;
-	while (*s == ' ' || *s == '\t' || *s == '\n' ||
-			*s == '\v' || *s == '\f' || *s == '\r')
-		s++;
-	sign(&s, &z);
-	while (*s >= '0' && *s <= '9')
+	num = 0;
+	minus = 1;
+	i = 0;
+	i = check_symbols(str);
+	if (str[i] == '+' || str[i] == '-')
 	{
-		nb = nb * 10 + (*s - '0');
-		s++;
+		if (str[i] == '-')
+			minus = -1;
+		i++;
 	}
-	if (nb >= 9223372036854775807)
-		return (z == -1 ? 0 : -1);
-	return (nb * z);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	if (num > 9223372036854775807 && minus == 1)
+		return (-1);
+	if (num > 9223372036854775807 && minus == -1)
+		return (0);
+	return (num * minus);
 }
